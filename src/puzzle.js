145008000183d41a583511c7b065114016a90f75
Evan Markowitz <emarkowitz1@gmail.com>
2019-05-19 14:43:26 -0600

    Add Turn Class

---
+++ src/puzzle.js
@@ -0,0 +1,10 @@
+class Puzzle {
+  constructor(category, numberOfWords, totalNumberOfLetters, firstWord, description, correctAnswer) {
+    this.category = category;
+    this.numberOfWords = numberOfWords;
+    this.totalNumberOfLetters = totalNumberOfLetters
+    this.firstWord = firstWord
+    this.description = description
+    this.correctAnswer = correctAnswer
+  }
+}
