f80da0ce89115e74a68aa01fb6217dea7708e99f
Evan Markowitz <emarkowitz1@gmail.com>
2019-05-20 15:25:47 -0600

    Build Puzzle Tests

---
+++ data/test_data_set.js
@@ -0,0 +1,186 @@
+const testData = {
+  response_code: {
+    version: '1.2',
+    termsofService: 'http://frontend.turing.io/projects/wheel-of-fortune.html',
+    features: {
+      wheel: 1,
+      puzzles: 1
+    },
+  },
+  wheel: [
+    900,
+    'BANKRUPT',
+    2500,
+    600,
+    700,
+    600,
+    650,
+    500,
+    700,
+    'BANKRUPT',
+    600,
+    550,
+    500,
+    600,
+    'BANKRUPT',
+    'LOSE A TURN',
+    700,
+    800,
+    500,
+    650,
+    500,
+    900
+  ],
+  puzzles: {
+    one_word_answers: {
+      date: 'Oct 07 2018',
+      puzzle_bank: [
+        {  
+          category: 'Around The House',
+          number_of_words: 1,
+          total_number_of_letters: 8,
+          first_word: 8, 
+          description:'Location or object(s) found within a typical house.',
+          correct_answer: 'Armchair',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 1,
+          total_number_of_letters: 7,
+          first_word: 7, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Beepers',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 1,
+          total_number_of_letters: 10,
+          first_word: 10, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Tamagotchi',
+        },
+        {  
+          category: 'Around The House',
+          number_of_words: 1,
+          total_number_of_letters: 6,
+          first_word: 6,
+          description: 'Location or object(s) found within a typical house.',
+          correct_answer: 'Teapot',
+        }
+      ]
+    },
+    two_word_answers: {
+      date: 'Oct 07 2018',
+      puzzle_bank: [
+        {  
+          category: 'Around The House',
+          number_of_words: 2,
+          total_number_of_letters: 11,
+          first_word: 7, 
+          description:'Location or object(s) found within a typical house.',
+          correct_answer: 'Amusing Yarn',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 2,
+          total_number_of_letters: 11,
+          first_word: 4, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Bowl Haircut',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 2,
+          total_number_of_letters: 13,
+          first_word: 7, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Britney Spears',
+        },
+        {  
+          category: 'Around The House',
+          number_of_words: 1,
+          total_number_of_letters: 12,
+          first_word: 7,
+          description: 'Location or object(s) found within a typical house.',
+          correct_answer: 'Beanbag Chair',
+        }
+      ]
+    },
+    three_word_answers: {
+      date: 'Oct 07 2018',
+      puzzle_bank: [
+        {  
+          category: 'Around The House',
+          number_of_words: 3,
+          total_number_of_letters: 10,
+          first_word: 3, 
+          description:'Location or object(s) found within a typical house.',
+          correct_answer: 'Hot Glue Gun',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 3,
+          total_number_of_letters: 12,
+          first_word: 6, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Tickle Me Elmo',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 3,
+          total_number_of_letters: 13,
+          first_word: 2, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'In Living Color',
+        },
+        {  
+          category: 'Around The House',
+          number_of_words: 1,
+          total_number_of_letters: 13,
+          first_word: 5,
+          description: 'Location or object(s) found within a typical house.',
+          correct_answer: 'Detox Foot Pads',
+        }
+      ]
+    },
+    four_word_answers: {
+      date: 'Oct 07 2018',
+      puzzle_bank: [
+        {  
+          category: 'Around The House',
+          number_of_words: 4,
+          total_number_of_letters: 17,
+          first_word: 4, 
+          description:'Location or object(s) found within a typical house.',
+          correct_answer: 'Roll Of Toilet Paper',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 4,
+          total_number_of_letters: 14,
+          first_word: 5, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'Saved By The Bell',
+        },
+        {  
+          category: 'The 90s',
+          number_of_words: 4,
+          total_number_of_letters: 18,
+          first_word: 3, 
+          description:'Puzzles pertaining to the decade in question.',
+          correct_answer: 'The Mickey Mouse Club',
+        },
+        {  
+          category: 'Around The House',
+          number_of_words: 1,
+          total_number_of_letters: 17,
+          first_word: 4,
+          description: 'Location or object(s) found within a typical house.',
+          correct_answer: 'Vase Full Of Flowers',
+        }
+      ]
+    }
+  }
+};
+
+export default testData
