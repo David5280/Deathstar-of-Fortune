9bb3bec1aff684622133fb5d75dc083ace5fa18b
Evan Markowitz <emarkowitz1@gmail.com>
2019-05-27 11:07:56 -0600

    Build buy vowel functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -12,5 +12,9 @@ export default {
     $('#player-1-score').text(player1)
     $('#player-2-score').text(player2)
     $('#player-3-score').text(player3)
+  },
+  enableBuyVowel(currentPlayer) {
+    console.log(currentPlayer)
+    currentPlayer >= 100 ? $('.buy-vowel').attr('disabled', false) : null
   }
 } 
